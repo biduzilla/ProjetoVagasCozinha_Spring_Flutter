ebadda10494b5eb71dc828fea7bffa773252ed68
Luíz Henrique de Carvalho Teles <rlyowlly19@gmail.com>
2022-08-31 13:47:14 -0300

    MySQL conectado

--- src/main/resources/meu_sqhema.sql
+++ src/main/resources/meu_sqhema.sql
@@ -1,26 +1,71 @@
+<<<<<<< Updated upstream
 CREATE TABLE TAB_USER (
     ID INTEGER PRIMARY KEY AUTO_INCREMENT,
     EMAIL VARCHAR(100),
     PASSWORD VARCHAR (100)
+=======
+create table tab_curriculum
+(
+    id             integer primary key auto_increment not null,
+    emailcontatocv varchar(255),
+    nome           varchar(255),
+    semestre       varchar(255),
+    sobre          varchar(255),
+    telefone       varchar(255),
+    user_id        integer,
+    vaga_id        integer,
+    constraint fk_tab_curriculum_tab_user foreign key(user_id) references tab_user(id),
+    constraint fk_tab_curriculum_tab_vaga foreign key(vaga_id) references tab_vaga(id)
 );
 
-CREATE TABLE TAB_CURRICULUM(
-    ID INTEGER PRIMARY KEY AUTO_INCREMENT,
-    USER_ID INTEGER REFERENCES TAB_USER(ID),
-    NOME VARCHAR(100),
-    TELEFONE VARCHAR(100),
-    EMAILCONTATOCV VARCHAR(100),
-    SOBRE VARCHAR(255)
+create table tab_user
+(
+    id integer primary key auto_increment not null,
+    admin boolean,
+    cv integer,
+    email varchar(100),
+    password varchar(100),
+    token varchar(255)
 );
 
-CREATE TABLE TAB_EXPERIENCIA(
-    ID INTEGER PRIMARY KEY AUTO_INCREMENT,
-    EXPERIENCIA_CURRICULUM_ID INTEGER REFERENCES TAB_CURRICULUM(ID),
-    EXPERIENCIA VARCHAR(255)
+create table tab_vaga
+(
+    id integer primary key auto_increment not null,
+    cargo varchar(255),
+    data_pedido date,
+    descricao varchar(255),
+    horario varchar(255),
+    local varchar(255),
+    remuneracao double,
+    user_id integer,
+    constraint fk_tab_vaga_tab_user foreign key(user_id) references tab_user(id)
 );
 
-CREATE TABLE TAB_QUALIFICACAO(
-    ID INTEGER PRIMARY KEY AUTO_INCREMENT,
-    QUALIFICACAO_CURRICULUM_ID INTEGER REFERENCES TAB_CURRICULUM(ID),
-    QUALIFICACAO VARCHAR(255)
+create table tab_candidaturas
+(
+    tab_user     integer not null,
+    candidaturas integer,
+    constraint fk_tab_candidaturas_tab_user foreign key (tab_user) references tab_user(id)
+>>>>>>> Stashed changes
+);
+
+create table tab_experiencias
+(
+    tab_curriculum integer not null,
+    experiencias   varchar(255),
+    constraint fk_tab_experiencias_tab_curriculum foreign key (tab_curriculum) references tab_curriculum(id)
+);
+
+create table tab_qualificacoes
+(
+    tab_curriculum integer not null,
+    qualificacoes  varchar(255),
+    constraint fk_tab_qualificacoes_tab_curriculum foreign key (tab_curriculum) references tab_curriculum(id)
+);
+
+create table tab_requisitos
+(
+    tab_vaga   integer not null,
+    requisitos varchar(255),
+    constraint fk_tab_requisitos_tab_vaga foreign key (tab_vaga) references tab_vaga(id)
 );
